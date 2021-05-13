9e5f4b4032db4b5999f4c1c53e35810ae49127fe
igor.balog <igor.balog@eng.it>
2021-05-13 15:44:11 +0200

    Excluding config.properties
    Changed how certificate file is read

--- src/main/java/it/eng/idsa/streamer/websocket/receiver/server/HttpWebSocketServerBean.java
+++ src/main/java/it/eng/idsa/streamer/websocket/receiver/server/HttpWebSocketServerBean.java
@@ -56,8 +56,10 @@ public class HttpWebSocketServerBean {
     }
 
     public void setup() {
-        URL url = getClass().getClassLoader().getResource(configuration.getString("server.ssl.key-store"));
-        Resource resource = Resource.newResource(url);
+//        URL url = getClass().getClassLoader().getResource(configuration.getString("server.ssl.key-store"));
+        Path pathSSL = Paths.get(configuration.getString("server.ssl.key-store"));
+    	logger.info("Creating resource using file {}", pathSSL);
+        Resource resource = Resource.newResource(pathSSL);
         Path keystorePath = null;
         if (null != FileRecreatorBeanExecutor.getInstance().getKeystorePath())
             keystorePath = Paths.get(FileRecreatorBeanExecutor.getInstance().getKeystorePath());
